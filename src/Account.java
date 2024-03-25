4ad499183e55ccc5f42b224533a86fb2c98b7460
Yaetrna <freyflorian727@protonmail.com>
2024-03-25 19:08:14 +0100

    01_01 Konto

---
+++ src/Account.java
@@ -0,0 +1,17 @@
+public class Account {
+    int accountID;
+    double accountBalance;
+    String accountOwner;
+
+    public void deposit(double amount) {
+        this.accountBalance += amount;
+    }
+
+    public void withdraw(double amount) {
+        this.accountBalance -= amount;
+    }
+
+    public double getAccountBalance() {
+        return this.accountBalance;
+    }
+}
---
+++ src/AccountMain.java
@@ -0,0 +1,28 @@
+public class AccountMain {
+    public static void main(String[] args) {
+        Account account = new Account();
+        account.accountID = 1007;
+        account.accountOwner = "Emil Braun";
+        account.accountBalance = 560.60;
+
+        printAccountInformation(account);
+
+        account.deposit(200.0);
+        account.withdraw(400.0);
+
+        System.out.println("Aktueller Kontostand: " + account.getAccountBalance());
+
+        Account account1 = new Account();
+        account1.accountID = 1009;
+        account1.accountOwner = "Helga Schmitt";
+        account1.accountBalance = 1340.00;
+
+        printAccountInformation(account1);
+    }
+
+    private static void printAccountInformation(Account account) {
+        System.out.println("Kunde: " + account.accountOwner);
+        System.out.println("Ihre Kontonummer lautet: " + account.accountID);
+        System.out.println("Aktueller Kontostand: " + account.accountBalance);
+    }
+}
